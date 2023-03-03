29214314b5000efc18a654d381a734b30ae232be
Joe Christian <100867785+joe-c-29b@users.noreply.github.com>
2023-03-02 16:01:34 -0800

    Update valueCounts.py

--- valueCounts.py
+++ valueCounts.py
@@ -2,3 +2,9 @@ def val_cou(df):
     for col in df.columns:
         print(df[col].value_counts())
 val_cou(house_cats)
+
+##alternate to output the total value count next to the column name
+def val_cou(df):
+    for col in df.columns:
+        print(df[col].value_counts().sum(), col)
+val_cou(house_cats)
